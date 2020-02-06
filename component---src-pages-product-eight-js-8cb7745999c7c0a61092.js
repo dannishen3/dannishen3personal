b52b410a7b239621c9e5060bf726e2e2615da934
Danni Shen <danni0shen@gmail.com>
2020-02-06 01:01:06 -0500

    Updates

---
+++ component---src-pages-product-eight-js-8cb7745999c7c0a61092.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{221:function(e,t,a){"use strict";a.r(t);var n=a(0),c=a.n(n),r=a(214),i=a(213),l=a(215);t.default=function(){return c.a.createElement(r.a,null,c.a.createElement(i.a,{title:"Page two"}),c.a.createElement(l.a,{id:8}))}}}]);
+//# sourceMappingURL=component---src-pages-product-eight-js-8cb7745999c7c0a61092.js.map
