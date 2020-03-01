717324f997f2de1ed3a76895fd74661820687b75
Scott Dikowitz <dikowitz@gmail.com>
2020-03-01 01:03:43 -0500

    underlines

--- src/components/ImageLink.js
+++ src/components/ImageLink.js
@@ -5,20 +5,21 @@ import React from 'react';
 export default function (props) {
     // const catalogItem = catalog[id];
     return (
-        <Link to={props.href} title={props.title} className="product-list-item rollover">
-            <img alt={props.alt_text} className="fade-in product-list-image" src={props.img_src}/>
-            <div className="product-list-item-container">
-            <figure className="product-list-image-container">
-            </figure>
-            </div>
-            <div className="product-list-item-info">
-            <div className="product-list-item-background"></div>
-            <div className="product-list-item-info-headers">
-                
-                <div className="product-list-item-name">{props.title}</div>
-                <div className="product-list-item-price">{props.description}</div>
-            </div>
-            </div>
-        </Link>
+        <div className="product-list-item rollover">
+            <Link to={props.href} title={props.title} className="product-list-item-link">
+                <img alt={props.alt_text} className="fade-in product-list-image" src={props.img_src}/>
+                <div className="product-list-item-container">
+                <figure className="product-list-image-container">
+                </figure>
+                </div>
+                <div className="product-list-item-info">
+                <div className="product-list-item-background"></div>
+                <div className="product-list-item-info-headers">
+                    <div className="product-list-item-name">{props.title}</div>
+                    <div className="product-list-item-price">{props.description}</div>
+                </div>
+                </div>
+            </Link>
+        </div>
     );
 }
