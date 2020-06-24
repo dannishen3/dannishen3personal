c01618a6e03277f1e27305667eb824fcf6cd3251
Danni Shen <danni0shen@gmail.com>
2020-06-24 02:26:32 -0400

    Updates

---
+++ component---src-pages-previous-js-ce604ca57633a332d0cd.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{213:function(e,a,t){"use strict";t.r(a);var n=t(0),l=t.n(n),c=t(205),s=t(204);a.default=function(){return l.a.createElement(c.a,null,l.a.createElement(s.a,{title:"EG"}),l.a.createElement("div",{class:"custom-page"},l.a.createElement("h1",{class:"custom-page-header page-title has-dash"},"EG"),l.a.createElement("div",{class:"custom-page-content"},l.a.createElement("p",null,"hello world"))))}}}]);
+//# sourceMappingURL=component---src-pages-previous-js-ce604ca57633a332d0cd.js.map
