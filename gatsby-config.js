1b6dd299f24bf750268b299acf6696941b2f1c6f
Danni Shen <danni0shen@gmail.com>
2020-08-08 23:26:22 -0400

    update description

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ module.exports = {
   pathPrefix: `/shop`,
   siteMetadata: {
     title: `nu-classics`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `nü-classics: re-defining classicality / contemporary, minimalist, among forms`,
+    author: `ScottD`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
